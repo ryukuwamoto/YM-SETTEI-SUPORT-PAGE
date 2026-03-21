1a05a667d6217075185a9b630d098731e66ec70b
ryukuwamoto <ipoi1026oda@gmail.com>
2026-03-21 17:30:27 +0900

    2026.3.21

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".slider").slick({
+    autoplay: true,
+    dots: false,
+    centerMode: true,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+  });
+});
