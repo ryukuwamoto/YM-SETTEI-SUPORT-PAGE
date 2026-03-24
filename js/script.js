78d360b9a1e87fc6323c92ccb58e213390d9f32c
ryukuwamoto <ipoi1026oda@gmail.com>
2026-03-24 17:33:24 +0900

    2026.3.24

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
 $(function () {
-  $(".slider").slick({
+  $(".d1-slider").slick({
     autoplay: true,
     dots: false,
     centerMode: true,
-    arrows: true,
-    slidesToShow: 1,
+    prevArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-d/side-arrow.png" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-d/side-arrow.png" class="slide-arrow next-arrow">',
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplaySpeed: 2000,
+    variableWidth: true,
+  });
+});
+
+$(function () {
+  $(".d2-slider").slick({
+    autoplay: true,
+    dots: false,
+    centerMode: true,
+    prevArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-d/side-arrow.png" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-d/side-arrow.png" class="slide-arrow next-arrow">',
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+  });
+});
+
+$(function () {
+  $(".f-slider").slick({
+    autoplay: true,
+    dots: true,
+    variableWidth: true,
+    prevArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-f/f-arrow.png" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-f/f-arrow.png" class="slide-arrow next-arrow">',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    dotsClass: "f-slide-dots",
   });
 });
