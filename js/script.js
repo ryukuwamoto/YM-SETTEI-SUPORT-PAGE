5c37aa883d2b692ac613eda30b15c797c7742f49
ryukuwamoto <ipoi1026oda@gmail.com>
2026-03-28 17:29:21 +0900

    2026.3.28

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function () {
   $(".d1-slider").slick({
-    autoplay: true,
+    autoplay: false,
     dots: false,
     centerMode: true,
     prevArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-d/side-arrow.png" class="slide-arrow prev-arrow">',
@@ -28,13 +28,27 @@ $(function () {
 
 $(function () {
   $(".f-slider").slick({
-    autoplay: true,
+    autoplay: false,
     dots: true,
     variableWidth: true,
     prevArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-f/f-arrow.png" class="slide-arrow prev-arrow">',
     nextArrow: '<img src="https://gigaplus.makeshop.jp/yemoba4818/settei-suport/image/sec-top-f/f-arrow.png" class="slide-arrow next-arrow">',
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 748,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ],
     autoplaySpeed: 2000,
     dotsClass: "f-slide-dots",
   });
